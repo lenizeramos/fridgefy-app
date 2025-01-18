e06951c26e3a465a6b26e0a81c253754fa71d21b
leniz <lenizemr@gmail.com>
2025-01-18 11:53:38 -0800

    create clerk login backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,27 @@
-import express from 'express';
+import express from "express";
+import "dotenv/config";
+import { clerkClient, getAuth, requireAuth } from "@clerk/express";
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, TypeScript!');
+app.get("/protected", requireAuth(), async (req, res) => {
+  const { userId } = getAuth(req);
+  console.log(userId)
+
+  if (!userId) {
+    res.status(401).json({ error: "Unauthorized" });
+    return;
+  }
+
+  const user = await clerkClient.users.getUser(userId);
+  res.json({ user });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello, TypeScript!");
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Server is runing at http://localhost:${PORT}`);
 });
