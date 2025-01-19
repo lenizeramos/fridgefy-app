a9d8e79230a40ca8174c85c47b4ba1d56229d3ea
LilyFoNo <lilyfono21@gmail.com>
2025-01-19 11:36:11 -0800

    context created

--- frontend/src/context/RecipiesContext.tsx
+++ frontend/src/context/RecipiesContext.tsx
@@ -17,11 +17,88 @@ type Recipes = {
   rating: number;
   reviewCount: number;
   mealType: string[];
-}
+};
 
 type RecipesState = {
-    recipies: Recipes[]
+  recipes: Recipes[];
+  selectedRecipe: Recipes | null;
+};
+
+type RecipesAction =
+  | { type: "setRecipes"; payload: Recipes[] }
+  | { type: "findRecipes"; payload: number };
+
+interface IRecipesContext {
+  state: RecipesState;
+  dispatch: React.Dispatch<RecipesAction>;
+  fetchData: () => Promise<void>;
 }
 
-type RecipesAction = 
-| {type: 'addRecipie'}
+const initialState: RecipesState = {
+  recipes: [],
+  selectedRecipe: null,
+};
+
+const RecipesReducer = (
+  state: RecipesState,
+  action: RecipesAction
+): RecipesState => {
+  switch (action.type) {
+    case "setRecipes":
+      return {
+        ...state,
+        recipes: action.payload,
+      };
+    case "findRecipes":
+      const selectedRecipe = state.recipes.find(
+        (recipe) => recipe.id === action.payload
+      );
+      return {
+        ...state,
+        selectedRecipe: selectedRecipe || null,
+      };
+    default:
+      return state;
+  }
+};
+
+const RecipesContext = createContext<IRecipesContext | undefined>(undefined);
+
+const RecipesProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [state, dispatch] = useReducer(RecipesReducer, initialState);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/fetch");
+      if (!response.ok) {
+        throw new Error("Error fetching data");
+      }
+      const data: Recipes[] = await response.json();
+      dispatch({ type: "setRecipes", payload: data });
+    } catch (error) {
+      throw new Error("Error fetching data");
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <RecipesContext.Provider value={{ state, dispatch, fetchData }}>
+      {children}
+    </RecipesContext.Provider>
+  );
+};
+
+const useRecipesContext = (): IRecipesContext => {
+  const context = useContext(RecipesContext);
+  if (!context) {
+    throw new Error("useRecipesContext must be used within a RecipesProvider ");
+  }
+  return context;
+};
+
+export { RecipesProvider, useRecipesContext };
