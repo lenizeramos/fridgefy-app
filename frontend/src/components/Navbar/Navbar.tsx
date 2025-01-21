7b79299162b2aa19d2b01a0cd9b0919070bf5395
Jose Trueba <joshu2991@hotmail.com>
2025-01-21 00:40:03 -0800

    shopping list merged with dev before push

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import logo from "/logo.png";
+import logo from "../../public/logo.png";
 import { SignedOut, SignedIn, SignOutButton } from "@clerk/clerk-react";
 import "./Navbar.scss";
 
