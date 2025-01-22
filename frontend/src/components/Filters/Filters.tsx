6886355f96c89105803c6e2b5c919c607c3cb361
LilyFoNo <lilyfono21@gmail.com>
2025-01-21 23:24:48 -0800

    search bar

--- frontend/src/components/Filters/Filters.tsx
+++ frontend/src/components/Filters/Filters.tsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
-import { useRecipesContext } from "../../context/RecipiesContext";
+import { useRecipesContext, Recipes } from "../../context/RecipiesContext";
 import "./Filters.scss";
 
-function Filters() {
+function Filters({ onFilter }: { onFilter: (searchTerm: string) => void }) {
   const { state } = useRecipesContext();
   const [tagSize, setTagSize] = useState(1);
   const [mealTypeSize, setMealTypeSize] = useState(1);
   const [cuisineSize, setCuisineSize] = useState(1);
   const [searchRecipe, setSearchRecipes] = useState("");
 
-  const handleSize = (setSizeFn: React.Dispatch<React.SetStateAction<number>>) => {
+  const handleSize = (
+    setSizeFn: React.Dispatch<React.SetStateAction<number>>
+  ) => {
     setSizeFn((prevSize) => (prevSize === 1 ? 5 : 1));
   };
 
@@ -19,7 +21,11 @@ function Filters() {
     setMealTypeSize(1);
   };
 
-  const searchRecipes = () => {};
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const searchTerm = event.target.value;
+    setSearchRecipes(searchTerm);
+    onFilter(searchTerm);
+  };
 
   return (
     <>
@@ -28,7 +34,9 @@ function Filters() {
           type="text"
           name=""
           id="searchRecipes"
+          value={searchRecipe}
           placeholder="Search Recipes"
+          onChange={handleChange}
         />
         <i className="bx bx-search-alt"></i>
         <div className="filters">
@@ -38,7 +46,7 @@ function Filters() {
               name="tags"
               id="tags-select"
               size={tagSize}
-              onClick={()=>handleSize(setTagSize)}
+              onClick={() => handleSize(setTagSize)}
               onBlur={handleBlur}
             >
               <option value="0">All Tags</option>
@@ -57,7 +65,7 @@ function Filters() {
               name="types"
               id="types-select"
               size={mealTypeSize}
-              onClick={()=>handleSize(setMealTypeSize)}
+              onClick={() => handleSize(setMealTypeSize)}
               onBlur={handleBlur}
             >
               <option value="0">All Meal Types</option>
@@ -76,7 +84,7 @@ function Filters() {
               name="cuisines"
               id="cuisines-select"
               size={cuisineSize}
-              onClick={()=>handleSize(setCuisineSize)}
+              onClick={() => handleSize(setCuisineSize)}
               onBlur={handleBlur}
             >
               <option value="0">All Cuisines</option>
--- frontend/src/components/RecipesList/RecipesList.tsx
+++ frontend/src/components/RecipesList/RecipesList.tsx
@@ -1,10 +1,21 @@
-import { useRecipesContext } from "../../context/RecipiesContext";
 import "./RecipesList.scss";
+import { useState } from "react";
+import { useRecipesContext } from "../../context/RecipiesContext";
 import Recipe from "../Recipe/Recipe";
 import Filters from "../Filters/Filters";
 
 function RecipesList() {
   const { state } = useRecipesContext();
+  const [filteredRecipes, setFilteredRecipes] = useState(state.recipes);
+
+  console.log(filteredRecipes);
+  const filterRecipes = (searchTerm: string) => {
+    const filtered = state.recipes.filter((recipe) =>
+      recipe.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredRecipes(filtered);
+  };
+
   return (
     <>
       <div className="mainContainer">
@@ -12,11 +23,18 @@ function RecipesList() {
           <div></div>
         </section>
         <section className="recipes">
-          <Filters />
+          <Filters onFilter={filterRecipes} />
           <div className="recipeContainer">
-            {state.recipes.map((recipe) => {
-              return <Recipe key={recipe.id} recipe={recipe} />;
-            })}
+            {filteredRecipes.length > 0 ? (
+              filteredRecipes.map((recipe) => {
+                return <Recipe key={recipe.id} recipe={recipe} />;
+              })
+            ) : (
+              <div className="tryAgain">
+                <h2 className="noFound">No recipes found</h2>
+                <span className="loader"></span>
+              </div>
+            )}
           </div>
         </section>
         <section className="wishList"></section>
