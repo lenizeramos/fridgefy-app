c6c38871387cd7d52614bec019acb8034b0e5e6f
LilyFoNo <lilyfono21@gmail.com>
2025-01-25 23:23:40 -0800

    some changes

--- frontend/src/components/RecipesList/RecipesList.tsx
+++ frontend/src/components/RecipesList/RecipesList.tsx
@@ -51,10 +51,7 @@ function RecipesList() {
       <div className="mainContainer">
         <MyFridgeComponent />
         <section className="recipes">
-          <Filters
-            filters={filters}
-            onFilterChange={handleFilterChange}
-          />
+          <Filters filters={filters} onFilterChange={handleFilterChange} />
           <div className="recipeContainer">
             {filteresRecipes.length > 0 ? (
               filteresRecipes.map((recipe) => {
