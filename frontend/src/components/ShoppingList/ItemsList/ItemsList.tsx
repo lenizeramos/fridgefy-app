fc4b565e8617bae47e06adf6f3196f7c31c2f3f9
LilyFoNo <lilyfono21@gmail.com>
2025-01-26 23:14:24 -0800

    itemlist styles

--- frontend/src/components/ShoppingList/ItemsList/ItemsList.tsx
+++ frontend/src/components/ShoppingList/ItemsList/ItemsList.tsx
@@ -5,25 +5,31 @@ function ItemsList() {
   const { state } = useShoppingListContext();
   const { items } = state;
 
-  return <div className="items-list-container">
-    <div className="items-list-title">Items to buy</div>
-    <div className="items-list-items">
-      {items.length > 0 ? (
-        items.map((item, index) => 
-          item.quantity > 0 && (
-              <div 
-                className="items-list-item" 
-                key={item.id || `item-${index}`}
-            >
-              <span className="item-quantity">{item.quantity}</span> {item.name}
-            </div>
+  return (
+    <div className="items-list-container">
+      <div className="items-list-title">
+        <p>Items to buy</p>
+      </div>
+      <div className="items-list-items">
+        {items.length > 0 ? (
+          items.map(
+            (item, index) =>
+              item.quantity > 0 && (
+                <div
+                  className="items-list-item"
+                  key={item.id || `item-${index}`}
+                >
+                  <span className="item-quantity">{item.quantity}</span>{" "}
+                  {item.name}
+                </div>
+              )
           )
-        )
-      ) : (
-        <div className="items-list-item">No items in the shopping list</div>
-      )}
+        ) : (
+          <div className="items-list-item">No items in the shopping list</div>
+        )}
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default ItemsList;
